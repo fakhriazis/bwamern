1033d5fc8a600ca7b664112226bffd63bf72ca9d
Fakhri Bashiri <fakhriazis994@gmail.com>
2021-08-10 15:10:54 +0700

    fix misstyped class + complete component parts/footer

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 import Button from "elements/Button";
-import BrandIcon from "parts/iconText";
+import BrandIcon from "parts/IconText";
 
 export default function Header(props) {
   const getNavLinkClass = (path) => {
