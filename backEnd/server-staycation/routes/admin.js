dfd37732be7bb442651c8b65180671e43bfe9ea7
Fakhri Bashiri <fakhriazis994@gmail.com>
2021-11-16 17:21:01 +0700

    CRUD Project Staycation Feature - Update
    create method for feature update module

--- backEnd/server-staycation/routes/admin.js
+++ backEnd/server-staycation/routes/admin.js
@@ -26,6 +26,7 @@ router.delete("/item/:id/delete", adminController.deleteItem);
 //endpoint detail item
 router.get("/item/show-detail-item/:itemId", adminController.viewDetailItem);
 router.post("/item/add/feature", uploadSingle, adminController.addFeature);
+router.put("/item/update/feature", uploadSingle, adminController.editFeature);
 
 //endpoint booking
 router.get("/booking", adminController.viewBooking);
