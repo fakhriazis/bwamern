dd61e2e7375d0ab7f0cc98e6bdc2bae1d95bf437
fakhriazis <fakhriazis994@gmail.com>
2022-01-07 10:06:42 +0700

    Login Project Staycation
    return res is important

--- backEnd/server-staycation/controllers/adminController.js
+++ backEnd/server-staycation/controllers/adminController.js
@@ -4,10 +4,51 @@ const Item = require("../models/Item");
 const Image = require("../models/Image");
 const Feature = require("../models/Feature");
 const Activity = require("../models/Activity");
+const Users = require("../models/Users");
 const fs = require("fs-extra");
 const path = require("path");
+const bcrypt = require("bcryptjs");
 
 module.exports = {
+
+  viewSignin: async (req, res) => {
+    try {
+      const alertMesaage = req.flash("alertMessage");
+      const alertStatus = req.flash("alertStatus");
+      const alert = { message: alertMesaage, status: alertStatus };
+      res.render("index", {
+        alert,
+        title: "Staycation | Login",
+      });
+    } catch (error) {
+      res.redirect("/admin/signin");
+    }
+  },
+
+  actionSignin: async (req,res) => {
+    try {
+      const {username, password} = req.body;
+      const user = await Users.findOne({ username: username });
+
+      if (!user) {
+        req.flash('alertMessage', 'User yang anda masukan tidak ada!!');
+        req.flash('alertStatus', 'danger');
+        return res.redirect('/admin/signin');
+      }
+
+      const isPasswordMatch = await bcrypt.compare(password, user.password);
+      if (!isPasswordMatch) {
+        req.flash('alertMessage', 'Password yang anda masukan tidak cocok!!');
+        req.flash('alertStatus', 'danger');
+        return res.redirect('/admin/signin');
+      }
+
+      res.redirect('/admin/dashboard');
+    } catch (error) {
+      return res.redirect('/admin/signin');
+    }
+  },
+
   viewDashboard: (req, res) => {
     res.render("admin/dashboard/view_dashboard", {
       title: "Staycation | Dashboard",
