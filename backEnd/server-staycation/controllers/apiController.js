0af4cfce55bca723d72b7859a0b02a516543060c
fakhriazis <fakhriazis994@gmail.com>
2022-01-25 13:05:09 +0700

    API Landing Page Project Staycation - 2

--- backEnd/server-staycation/controllers/apiController.js
+++ backEnd/server-staycation/controllers/apiController.js
@@ -1,6 +1,33 @@
+const Item = require('../models/Item');
+const Treasure = require('../models/Activity');
+const Traveller = require('../models/Booking');
+
+
+
 module.exports = {
-    landingPage: (req,res) => {
-        const message = 'Hello Json';
-        res.status(200).json({message})
+    landingPage: async (req,res) => {
+        try {
+            const mostPicked = await Item.find()
+            .select('_id title country city price unit imageId')
+            .limit(5)
+            .populate({path: 'imageId', select: '_id imageUrl'})
+
+            const traveller = Traveller.find()
+            const treasure = Treasure.find()
+            const city = Item.find()
+
+
+
+            res.status(200).json({
+                hero: {
+                    travelers: (await traveller).length,
+                    treasure: (await treasure).length,
+                    cities: (await city).length
+                },
+                mostPicked
+            })
+        } catch (error) {
+            
+        }
     }
 }
--- backEnd/server-staycation/models/Item.js
+++ backEnd/server-staycation/models/Item.js
@@ -26,6 +26,10 @@ const itemSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  unit: {
+    type: String,
+    default: 'night'
+  },
   categoryId: {
     type: ObjectId,
     ref: "Category",
