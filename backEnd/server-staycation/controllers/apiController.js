0a6ef5a046980c7aa8e6d675ced3793428021dba
fakhriazis <fakhriazis994@gmail.com>
2022-01-25 13:24:32 +0700

    API Landing Page Project Staycation - 3

--- backEnd/server-staycation/controllers/apiController.js
+++ backEnd/server-staycation/controllers/apiController.js
@@ -1,6 +1,7 @@
 const Item = require('../models/Item');
 const Treasure = require('../models/Activity');
 const Traveller = require('../models/Booking');
+const Category = require('../models/Category');
 
 
 
@@ -12,11 +13,23 @@ module.exports = {
             .limit(5)
             .populate({path: 'imageId', select: '_id imageUrl'})
 
+            const category = await Category.find()
+            .select('_id name')
+            .limit(3)
+            .populate({
+                path: 'itemId', 
+                select: '_id title country city isPopular imageId',
+                perDocumentLimit: 4,
+                populate: {
+                    path: 'imageId', 
+                    select: '_id imageUrl',
+                    perDocumentLimit: 1
+                }
+            })
+
             const traveller = Traveller.find()
             const treasure = Treasure.find()
             const city = Item.find()
-
-
 
             res.status(200).json({
                 hero: {
@@ -24,7 +37,8 @@ module.exports = {
                     treasure: (await treasure).length,
                     cities: (await city).length
                 },
-                mostPicked
+                mostPicked,
+                category
             })
         } catch (error) {
             
