74668f1c296272db4f421d6d1e6e7e0790cffeb7
fakhriazis <fakhriazis994@gmail.com>
2022-01-26 17:19:36 +0700

    API Booking Project Staycation - 2

--- backEnd/server-staycation/controllers/apiController.js
+++ backEnd/server-staycation/controllers/apiController.js
@@ -3,6 +3,8 @@ const Treasure = require('../models/Activity');
 const Traveller = require('../models/Booking');
 const Category = require('../models/Category');
 const Bank = require('../models/Bank');
+const Booking = require('../models/Booking');
+const Member = require('../models/Member');
 
 
 
@@ -133,9 +135,53 @@ module.exports = {
             phoneNumber === undefined ||
             accountHolder === undefined ||
             bankFrom === undefined ){
-            return res.status(404).json({ message: "Lengkapi semua field" })
+            return res.status(404).json({ message: "Lengkapi semua field" });
         }
 
-        res.status(201).json({message: 'Success Booking'})
+        const item = await Item.findOne({ _id: idItem });
+
+        if (!item){
+            return res.status(404).json({ message: "item not found"});
+        }
+
+        item.sumBooking += 1;
+
+        await item.save();
+
+        let total = item.price * duration;
+        let tax = total * 0.10;
+
+        const invoice = Math.floor(1000000 + Math.random() * 9000000);
+
+        const member = await Member.create({
+            firstName,
+            lastName,
+            email,
+            phoneNumber
+        });
+
+        const newBooking = { 
+            invoice,
+            bookingStartDate,
+            bookingEndDate,
+            total: total += tax,
+            itemId: {
+                _id: item.id,
+                title: item.title,
+                price: item.price,
+                duration: duration
+            },
+
+            memberId: member.id,
+            payments: {
+                proofPayment: `images/${req.file.filename}`,
+                bankFrom: bankFrom,
+                accountHolder: accountHolder
+            }
+        }
+
+        const booking = await Booking.create(newBooking);
+
+        res.status(201).json({message: 'Success Booking', booking})
     }
 }
--- backEnd/server-staycation/models/Booking.js
+++ backEnd/server-staycation/models/Booking.js
@@ -63,7 +63,7 @@ const bookingSchema = new mongoose.Schema({
       },
       status: {
         type: String,
-        required: true,
+        default: 'Proses',
       },
     },
 });
