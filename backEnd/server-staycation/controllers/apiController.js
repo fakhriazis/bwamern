7fd771a4ebcf9d6bdd993d1838eadf03f30ea062
fakhriazis <fakhriazis994@gmail.com>
2022-01-26 12:07:42 +0700

    API Detail Page Project Staycation

--- backEnd/server-staycation/controllers/apiController.js
+++ backEnd/server-staycation/controllers/apiController.js
@@ -2,6 +2,7 @@ const Item = require('../models/Item');
 const Treasure = require('../models/Activity');
 const Traveller = require('../models/Booking');
 const Category = require('../models/Category');
+const Bank = require('../models/Bank');
 
 
 
@@ -67,6 +68,35 @@ module.exports = {
         } catch (error) {
             console.log(error);
             res.status(500).json({message: "Internal server error"})
+        }
+    },
+
+    detailPage: async (req, res) => {
+        try {
+            const {id} = req.params;
+            const item = await Item.findOne({ _id:id })
+            .populate({path: 'featureId', select: '_id name qty imageUrl'})
+            .populate({path: 'activityId', select: '_id name type imageUrl'})
+
+            const bank = await Bank.find();
+
+            testimonial = {
+                _id: "asd1293uasdads1",
+                imageUrl: "images/testimonial1.jpg",
+                name: "Happy Family",
+                rate: 4.55,
+                content: "What a great trip with my family and I should try again next time soon ...",
+                familyName: "Angga",
+                familyOccupation: "Product Designer"
+            }
+
+            res.status(200).json({
+                ...item._doc,
+                bank,
+                testimonial
+            })
+        } catch (error) {
+            
         }
     }
 }
